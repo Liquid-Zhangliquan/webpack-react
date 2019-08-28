ad10b6fdec9fe43ab469adc11684ab6fef0a4dcb
Liquid-Zhangliquan <1063754495@qq.com>
2019-08-28 21:54:19 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const config = {
+    entry: path.resolve(__dirname, 'app/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
